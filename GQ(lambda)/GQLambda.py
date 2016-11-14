a759819bceb53a4e65f7b0b473e7e8fd65c6719e
Zhiwei Han <hanzw356255531@icloud.com>
2016-11-14 14:33:07 +0100

    Format Modification

--- GQ(lambda)/GQLambda.py
+++ GQ(lambda)/GQLambda.py
@@ -1,11 +1,22 @@
-__auther__ = "Zhiwei"
 import numpy as np
 import random as rd
 import sys
+from copy import deepcopy
+
+__auther__ = "Zhiwei"
+
 
 class GQLambda(object):
     """docstring for GQLambda"""
-    def __init__(self, alpha, gamma, lambda_back, eta, epsilon, action_space):
+
+    def __init__(self,
+                 alpha,
+                 gamma,
+                 lambda_back,
+                 eta,
+                 epsilon,
+                 action_space
+                 ):
         super(GQLambda, self).__init__()
 
         self.alpha = alpha
@@ -14,34 +25,48 @@ class GQLambda(object):
         self.eta = eta
         self.epsilon = epsilon
         self.state_space = action_space.keys()
-        self.action_space = action_space
+        self.action_space = deepcopy(action_space)
 
-        self.num_qfunc = reduce(lambda x, y: x + len(y), action_space.values(), 0)
-        self.theta = np.random.rand(self.num_qfunc)
-        self.w = np.zeros(self.num_qfunc)
-        self.e = np.zeros(self.num_qfunc)
+        self.num_element_qfunc = reduce(
+            lambda x, y: x + len(y),
+            self.action_space.value(),
+            0
+        )
+        self.theta = np.random.rand(self.num_element_qfunc)
+        self.w = np.zeros(self.num_element_qfunc)
+        self.e = np.zeros(self.num_element_qfunc)
 
     def _m_Learn(self, phi, phi_bar, step_reward, rho, i_unknown):
         try:
-            if len(self.theta) != len(phi_bar):
+            if (
+                len(self.theta) != len(phi_bar) or
+                len(self.theta) != len(phi)
+            ):
                 raise ValueError("Dimension not same")
         except ValueError as e:
             print e
             print "theta dimension is", len(self.theta)
             print "phi_bar dimension is", len(phi_bar)
             sys.exit(0)
-        delta = step_reward + self.gamma * np.dot(self.theta, phi_bar)
+        delta = step_reward + self.gamma * np.dot(
+            self.theta,
+            phi_bar
+        )
         delta -= np.dot(self.theta, phi)
 
         self.e = rho * self.e + i_unknown * phi
 
-        dot_w_e = np.dot(self.w, self.e);
-        dot_w_phi = np.dot(self.w, phi);
-        gradient = delta * self.e - self.gamma * (1 - self.lambda_back) * dot_w_e * phi_bar
+        dot_w_e = np.dot(self.w, self.e)
+        dot_w_phi = np.dot(self.w, phi)
+        gradient = delta * self.e - (
+            self.gamma * (1 - self.lambda_back) * dot_w_e * phi_bar
+        )
 
         self.theta += self.alpha * gradient
-        self.w += self.alpha * self.eta * (delta * self.e - dot_w_phi * phi)
-        self.e *= self.gamma * self.lambda_back;
+        self.w += self.alpha * self.eta * (
+            delta * self.e - dot_w_phi * phi
+        )
+        self.e *= self.gamma * self.lambda_back
 
     def _m_GreedyPolicy(self, state):
         state = tuple(state)
@@ -51,21 +76,27 @@ class GQLambda(object):
 
         max_value = -float("inf")
         for action in self.action_space[state]:
-            action_value = np.dot(self.theta, self.__state_action_to_feature(state, action))
+            action_value = np.dot(
+                self.theta,
+                self.__discret_state_action_to_feature(state, action)
+            )
             if max_value < action_value:
                 return_action = action
                 max_value = action_value
 
         return return_action
-        
-    def __state_action_to_feature(self, state, action):
+
+    def __discret_state_action_to_feature(self, state, action):
         """Transfer observation to feature space(only for Mountain Car!) """
         count = 0
         for state_index in self.state_space:
             if state_index != state:
-                count += len(list(self.action_space[state_index]))
+                count += len(list(
+                    self.action_space[state_index]
+                )
+                )
 
         count += list(self.action_space[state]).index(action)
-        feature = np.zeros(self.num_qfunc)
+        feature = np.zeros(self.num_element_qfunc)
         feature[count] = 1
         return feature
--- GQ(lambda)/StateActionSpace.py
+++ GQ(lambda)/StateActionSpace.py
@@ -1,54 +1,68 @@
-__auther__ = "Zhiwei"
 import numpy as np
 import itertools
 import sys
 import math
-import copy
+from copy import deepcopy
+
+__auther__ = "Zhiwei"
+
 
 class StateActionSpace(object):
     """docstring for StateActionSpace"""
+
     def __init__(self, continuous_space, precise, actions):
         super(StateActionSpace, self).__init__()
-        self.continuous_space = continuous_space
-        self.precise = precise
-        self.actions = actions
-        self.num_zero_entry = reduce(lambda x, y : x * y, precise, 1) * len(actions)
+        self.continuous_space = deepcopy(continuous_space)
+        self.precise = deepcopy(precise)
+        self.actions = deepcopy(actions)
 
-        self.state_space, self.lower_band, self.upper_band, self.unit = self.__discret_space()
+        self.lower_band = deepcopy(list(continuous_space)[0])
+        self.upper_band = deepcopy(list(continuous_space)[1])
+        self.divide_unit = self.__divide_unit_compute()
+
+        self.state_space = self.__discret_space()
         self.action_space = self.__state_action_space_generation()
+        self.element_num_qfunc = reduce(lambda x, y: x + len(y),
+                                        self.action_space.values(),
+                                        0
+                                        )
 
+    def __divide_unit_compute(self):
+        precise = [float(i) for i in self.precise]
+        unit = (
+            np.array(self.upper_band) - np.array(self.lower_band)
+        ) / np.array(precise)
+        return unit
 
     def __discret_space(self):
-        lower_band = list(self.continuous_space)[0]
-        upper_band = list(self.continuous_space)[1]
-        precise = [float(i) for i in self.precise]
-        unit = (np.array(upper_band) - np.array(lower_band)) / np.array(precise)
-
         sub_precise = []
         for i in self.precise:
             sub_precise.append([j for j in xrange(0, i)])
-        return itertools.product(*sub_precise), lower_band, upper_band, unit
-
+        return itertools.product(*sub_precise)
 
     def __state_action_space_generation(self):
         """ Hard code:
             generate the action space only for mountain car """
-        return {i : self.actions for i in self.state_space}
-
+        return {i: self.actions for i in self.state_space}
 
     def _m_state_continuous_to_discret(self, input_state):
         try:
             if len(self.upper_band) != len(input_state):
                 raise ValueError("The continous state illegal")
             for i in xrange(0, len(input_state)):
-                if not(self.lower_band[i] <= input_state[i] <= self.upper_band[i]):
+                if not(
+                        self.lower_band[i] <=
+                        input_state[i] <=
+                        self.upper_band[i]
+                ):
                     raise ValueError("The continous state illegal")
         except ValueError as e:
             print e
             sys.exit()
 
-        discret_state = input_state
-        discret_state = (discret_state - np.array(self.lower_band)) / self.unit
+        discret_state = np.array(deepcopy(input_state))
+        discret_state -= np.array(self.lower_band)
+        discret_state /= self.divide_unit
         discret_state = [int(math.floor(i)) for i in discret_state]
 
         for i in xrange(0, len(input_state)):
@@ -56,19 +70,18 @@ class StateActionSpace(object):
                 discret_state[i] = self.precise[i] - 1
         return discret_state
 
-
     def _m_discret_state_to_feature(self, discret_state, action):
-        feature = np.zeros(self.num_zero_entry)
-        new_state = copy.deepcopy(discret_state)
+        feature = np.zeros(self.element_num_qfunc)
+        new_state = deepcopy(discret_state)
         new_state.append(action)
-        new_precise = self.precise
+        new_precise = deepcopy(self.precise)
         new_precise.append(len(self.actions))
         precise = new_precise
 
         count = 0
         for i, j in zip(new_precise, new_state):
             precise.pop(0)
-            count += j * reduce(lambda x, y:x * y, precise, 1)
+            count += j * reduce(lambda x, y: x * y, precise, 1)
         try:
             if count > len(feature):
                 raise ValueError("Index out of range")
@@ -82,8 +95,17 @@ class StateActionSpace(object):
 
 
 if __name__ == '__main__':
-    state_action_space = StateActionSpace(([-1.2, -0.07], [0.6, 0.07]), [3, 3], [0, 1])
-    discret_state = state_action_space._m_state_continuous_to_discret([0, 0.07])
+    state_action_space = StateActionSpace(
+        ([-1.2, -0.07], [0.6, 0.07]),
+        [3, 3],
+        [0, 1]
+    )
+    discret_state = state_action_space._m_state_continuous_to_discret(
+        [0, 0.07]
+    )
     print discret_state
-    feature_vector = state_action_space._m_discret_state_to_feature(discret_state, 0)
+    feature_vector = state_action_space._m_discret_state_to_feature(
+        discret_state,
+        0
+    )
     print feature_vector
