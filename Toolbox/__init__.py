a6da8058f89969ee0ad2f5e2e6e7f2a07c1bc109
Zhiwei Han <hanzw356255531@icloud.com>
2016-12-04 21:30:00 +0100

    Modulerise and documented

---
+++ Toolbox/__init__.py
@@ -0,0 +1,4 @@
+from StateActionSpace_MountainCar import StateActionSpace_MountainCar
+from StateActionSpace_CartPole import StateActionSpace_CartPole
+from GGQLambda import GGQLambda
+from RGGQLambda import RGGQLambda
