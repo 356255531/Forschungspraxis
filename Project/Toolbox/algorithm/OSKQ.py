11f0297833da3291e532f88dec762df097d6e13d
Zhiwei Han <hanzw356255531@icloud.com>
2017-04-02 14:45:07 +0200

    Component finished and cleanup

--- Project/Toolbox/algorithm/OSKQ.py
+++ Project/Toolbox/algorithm/OSKQ.py
@@ -1,434 +1,186 @@
 import numpy as np
-import numpy.random as rd
-import sys
-from copy import deepcopy
-import pdb
-
-__auther__ = "Zhiwei"
+import random as rd
 
 
 class OSKQ(object):
+    """docstring for OSKQ"""
+
     def __init__(self,
                  mu_1,
                  mu_2,
-                 state_action_space,
-                 env,
-                 alpha=0.1,
-                 gamma=0.9,
-                 e_factor=0.9,
-                 epsilon=0.3,
-                 sigma=1,
-                 num_episode=500,
-                 max_step_per_episode=200,
-                 reward_threashold=float("inf"),
+                 alpha,
+                 gamma,
+                 e_lambda,
+                 action_space,
+                 observation_dim,
+                 sigma
                  ):
-        """
-            Input(constructor):
-                alpha: float, learning rate,
-                gamma: float, discount factor,
-                e_factor: float, eligibility factor,
-                eta: float,
-                epsilon: float, greedy factor
-                action_space: dict, {state1(tuple):actions1(list),....} """
         super(OSKQ, self).__init__()
-        # Input parameter initialization
-        self.__alpha = alpha
-        self.__gamma = gamma
-        self.__e_factor = e_factor
-        self.__epsilon = epsilon
-        self.__sigma = sigma
-        self.__mu_1 = mu_1
-        self.__mu_2 = mu_2
-        self.__state_action_space = state_action_space
-        self.__state_space = \
-            state_action_space.get_state_space()
-        self.__action_space = \
-            state_action_space.get_action_space()
-        self.__env = env
-        self.__num_episode = num_episode
-        self.__max_step_per_episode = max_step_per_episode
-        self.__reward_threashold = reward_threashold
+        self.mu_1 = mu_1
+        self.mu_2 = mu_2
+        self.alpha = alpha
+        self.gamma = gamma
+        self.e_lambda = e_lambda
+        self.action_space = action_space
+        self.observation_dim = observation_dim
+        self.sigma = sigma
 
-        # Build in paramater initialization
-        self.__feature_vector_dim = \
-            state_action_space.get_feature_vector_dim()
-
-        self.__dict = self._init_dict()
-        self.__theta = self._init_theta()
-
-        self.__e = self._init_eligibility_trace()
-
-        self.__total_reward_history = []
-        self.__theta_diff_history = []
-        self.__max_total_reward = -float("inf")
-
-    def _init_dict(self):
-        init_dict = np.empty(
-            shape=(0, self.__feature_vector_dim)
+        self.dict = np.empty(
+            shape=(0, observation_dim + 1)
         )
-        return init_dict
-
-    def _init_theta(self):
-        theta_size = self.__dict.shape[0]
-        theta = np.empty((theta_size, 1))
-        return theta
-
-    def _init_eligibility_trace(self):
-        dict_size = self.__dict.shape[0]
-        eligitility_trace = np.zeros(
-            (dict_size, 1)
+        self.theta = np.empty(
+            shape=(0, 1)
         )
-        return eligitility_trace
-
-    def _trans_observation_to_state(self, observation):
-        state = self.__state_action_space.trans_observation_to_state(
-            observation
+        self.eligibility_trace = np.empty(
+            shape=(0, 1)
         )
-        return state
 
-    def _trans_state_action_to_feature_vector(
-        self,
-        state,
-        action
+    def _m_Learn(
+            self,
+            observation,
+            action,
+            observation_bar,
+            action_bar,
+            reward
     ):
-        feature_vector = self.__state_action_space.trans_state_action_to_feature_vector(
-            state,
-            action
-        )
-        return feature_vector
+        feature_vector = np.append(observation, action)
 
-    def _add_feature_vector_to_dict(
+        feature_vector_bar = np.append(observation_bar, action_bar)
+
+        delta = reward + self.gamma * self.get_Q_Func(feature_vector_bar) - \
+            self.get_Q_Func(feature_vector)
+        # print feature_vector
+        # print "dict", self.dict
+        # print self.dict
+        # print self.eligibility_trace
+        self.eligibility_trace = \
+            self.gamma * self.e_lambda * self.eligibility_trace \
+            + self.select_function(feature_vector, self.mu_2) * \
+            self.gaussian_kernel(feature_vector)
+        self.theta += self.alpha * delta * self.eligibility_trace
+        self.dict_update(feature_vector)
+
+    def get_Q_Func(
         self,
         feature_vector
     ):
-        self.__dict = np.concatenate(
-            (self.__dict, feature_vector.T),
-            axis=0
+        Q_func = np.sum(
+            self.theta * self.select_function(feature_vector, self.mu_2) *
+            self.gaussian_kernel(feature_vector)
         )
-        self.__e = np.concatenate(
-            (self.__e, np.zeros((1, 1))),
-            axis=0
-        )
-        self.__theta = np.concatenate(
-            (self.__theta, rd.random_sample((1, 1))),
-            axis=0
-        )
+        return Q_func
 
-    def _kernel_select_func(
+    def dict_update(
         self,
-        phi,
-        phi_bar,
-        mu
+        feature_vector
     ):
-        phi_copy = deepcopy(phi)
-        phi_copy[:, 1] *= 3
-        phi_bar_copy = deepcopy(phi_bar)
-        phi_bar_copy[1, 0] *= 3
-        dist = 2 - 2 * self._gaussian_kernel(phi_copy, phi_bar_copy)
-        kernel_select_func = (dist < mu).astype(int)
+        already_in_dict = np.sum(self.select_function(feature_vector, self.mu_1))
 
-        return kernel_select_func
+        # pdb.set_trace()
+        if already_in_dict == 0:
+            self.dict = np.concatenate(
+                (self.dict, feature_vector.reshape((1, self.observation_dim + 1))),
+                axis=0
+            )
+            self.theta = np.concatenate(
+                (self.theta, np.array(np.random.random_sample()).reshape((1, 1))),
+                axis=0)
+            self.eligibility_trace = np.concatenate(
+                (self.eligibility_trace, np.zeros(shape=(1, 1))),
+                axis=0)
 
-    def _gaussian_kernel(
-        self,
-        phi,
-        phi_bar
-    ):
-        phi_size = phi.shape[0]
+    def _m_GreedyPolicy(self, observation, epsilon):
+        """
+        feature_vector size (3,0)"""
+        if np.random.sample < epsilon:
+            return rd.sample(self.action_space, 1)[0]
 
-        one_vector = np.ones((phi_size, 1))
-        gaussian_kernel_input = phi - one_vector.dot(
-            phi_bar.T
-        )
-        gaussian_kernel_input = gaussian_kernel_input.dot(
-            gaussian_kernel_input.T
-        )
+        max_Q_func = -float("inf")
+        for action in self.action_space:
+            feature_vector = np.append(observation, action)
+            if_exist_in_dict = np.sum(self.select_function(feature_vector, self.mu_1))
+            if not if_exist_in_dict:
+                return action
 
-        gaussian_kernel_input = np.array([np.diag(
-            gaussian_kernel_input
-        )]
-        ).T
+            Q_func = self.get_Q_Func(feature_vector)
+            if Q_func > max_Q_func:
+                # pdb.set_trace()
+                max_Q_func = Q_func
+                greedy_action = action
+
+        return greedy_action
+
+    def gaussian_kernel(self, feature_vector):
+        dict_feature_num = self.dict.shape[0]
+
+        one_vector = np.ones((dict_feature_num, 1))
+
+        minuser = np.dot(one_vector, feature_vector.reshape((1, self.observation_dim + 1)))
+
+        differ_matrix = self.dict - minuser
+
+        gaussian_kernel_input = np.dot(differ_matrix, differ_matrix.T)
+
+        gaussian_kernel_input = np.diag(gaussian_kernel_input).reshape((dict_feature_num, 1))
 
         gaussian_kernel = np.exp(
-            -gaussian_kernel_input / (self.__sigma * self.__sigma * 2.0)
-        ) / (self.sigma * (2 * np.pi) ** 0.5)
+            -gaussian_kernel_input / (self.sigma * self.sigma * 2.0)
+        )
+
         return gaussian_kernel
 
-    def _take_action(self, state, action):
+    def select_function(self, feature_vector, mu):
+        selection = (2 - 2 * self.gaussian_kernel(feature_vector)) < mu
+        selection = selection.astype(int)
+        return selection
 
-        # observation, step_reward, done, info = self.__env.step(
-        #     action
-        # )
-        # state_bar = self._trans_observation_to_state(observation)
 
-        feature_vector = self._trans_state_action_to_feature_vector(
-            state,
-            action
-        )
-        state_bar = state
-        kernel_select_func = self._kernel_select_func(
-            self.__dict,
-            feature_vector,
-            self.__mu_2
-        )
+if __name__ == '__main__':
+    import gym
+    from StateActionSpace_MountainCar import StateActionSpace_MountainCar
 
-        count = 0
-        while list(kernel_select_func) == list(
-                self._kernel_select_func(
-                    self.__dict,
-                    self._trans_state_action_to_feature_vector(
-                        state_bar,
-                        action
-                    ),
-                    self.__mu_2
-                )
-        ):
-            if count == 20:
-                return state_bar, step_reward, done, info, True
-            observation_bar, step_reward, done, info = self.__env.step(
-                action
-            )
-            state_bar = self._trans_observation_to_state(
-                observation_bar
-            )
-            count += 1
-        return state_bar, step_reward, done, info, False
+    # Learning Parameter
+    precise = [8, 8]
 
-    def _if_dict_empty(self):
-        if self.__dict.shape[0] == 0:
-            return True
-        else:
-            return False
+    learning_rate = 0.1
+    discount_factor = 0.9
+    discount_of_learning_rate = 0.9
+    eligibility_factor = 0.9
+    regularize_factor = 0.00003  # 0.0001
+    epsilon = 0.1
 
-    def _get_e(self):
-        return self.__e
+    # Parameter OSK-Q
+    mu_1 = 0.04
+    mu_2 = 0.08
+    sigma = 1
+    # Macro
+    NUM_STEP = 200
+    NUM_EPISODE = 500
+    REWARD_THREASHOLD = -100
+    # Definition of dependencies
+    env = gym.make('MountainCar-v0')
 
-    def get_epsilon_greedy_action(
-        self,
-        state,
-        epsilon
-    ):
-        # pdb.set_trace()
-        if rd.random_sample() < epsilon or \
-                self._if_dict_empty():
-            action = self._get_random_action()
-            return action
+    observation_space = (
+        env.observation_space.low,
+        env.observation_space.high
+    )
 
-        max_value = -float("inf")
-        for action in self.__action_space:
-            # pdb.set_trace()
-            Qfunc = self._get_Qfunc(
-                state,
-                action
-            )
-            if Qfunc is None:
-                action = self._get_random_action()
-                return action
-            if max_value < Qfunc:
-                return_action = deepcopy(action)
-                max_value = Qfunc
+    MountainCar_universal_action_space = [i for i in xrange(0, env.action_space.n)]
+    state_action_space = StateActionSpace_MountainCar(
+        observation_space,
+        precise,
+        MountainCar_universal_action_space
+    )
 
-        if 'return_action' not in locals():
-            return_action = rd.choice(
-                self.__action_space
-            )
+    learning_agent_OSKQ = OSKQ(
+        mu_1,
+        mu_2,
+        learning_rate,
+        discount_factor,
+        eligibility_factor,
+        epsilon,
+        state_action_space.action_space,
+        sigma
+    )
 
-        return return_action
-
-    def get_optimal_action(self, state):
-        action = self.get_epsilon_greedy_action(state, 0)
-        return action
-
-    def _get_Qfunc(
-            self,
-            state,
-            action
-    ):
-        # pdb.set_trace()
-        # pdb.set_trace()
-        feature_vector = self._trans_state_action_to_feature_vector(
-            state,
-            action
-        )
-        # pdb.set_trace()
-
-        kernel_select_func = self._kernel_select_func(
-            self.__dict,
-            feature_vector,
-            self.__mu_2
-        )
-
-        if np.count_nonzero(kernel_select_func) == 0:
-            self._update_dict(state, action)
-            kernel_select_func = self._kernel_select_func(
-                self.__dict,
-                feature_vector,
-                self.__mu_2
-            )
-
-        gaussian_kernel = self._gaussian_kernel(
-            self.__dict,
-            feature_vector
-        )
-
-        Qfunc = kernel_select_func * \
-            self.__theta * gaussian_kernel
-
-        Qfunc = np.sum(Qfunc)
-
-        return Qfunc
-
-    def _update_dict(self, state, action):
-        feature_vector = self._trans_state_action_to_feature_vector(
-            state, action
-        )
-
-        if_feature_vector_redundent = self._kernel_select_func(
-            self.__dict,
-            feature_vector,
-            self.__mu_1
-        )
-        # pdb.set_trace()
-        if np.sum(if_feature_vector_redundent) == 0:
-            for action in self.__action_space:
-                feature_vector = self._trans_state_action_to_feature_vector(
-                    state, action
-                )
-                self._add_feature_vector_to_dict(feature_vector)
-
-    def _get_dict(self):
-        return self.__dict
-
-    def _get_theta(self):
-        return self.__theta
-
-    def _update_eligibility_trace(
-        self,
-        state,
-        action
-    ):
-        feature_vector = self._trans_state_action_to_feature_vector(
-            state,
-            action
-        )
-
-        # pdb.set_trace()
-        eligitility_trace = self.__gamma * self.__e_factor * self.__e + \
-            self._kernel_select_func(
-                self.__dict,
-                feature_vector,
-                self.__mu_2) * \
-            self._gaussian_kernel(
-                self.__dict,
-                feature_vector
-            )
-        # pdb.set_trace()
-        return eligitility_trace
-
-    def _update_weights(self, delta):
-        theta = self.__theta + self.__alpha * delta * self.__e
-        return theta
-
-    def _init_enviroment(self):
-        observation = self.__env.reset()
-        state = self._trans_observation_to_state(observation)
-        return state
-
-    def learn(self):
-        """ Update the learned parameters
-            Input:
-                phi: integer array, feature vector[0,0,0..1...0,0]
-                phi_bar: integer array, feature vector[0,0,0..1...0,0]
-                transient_reward:
-                 float, reward each step
-                rho: float, the probability expectation of action
-                i: float, set of interest for s_t, a_t [0,1] """
-
-        for num_episode in range(self.__num_episode):
-            # print "Episode: ", num_episode
-
-            state = self._init_enviroment()
-
-            total_reward = 0
-            theta_previous = deepcopy(self.__theta)
-            # pdb.set_trace()
-
-            for num_step in range(self.__max_step_per_episode):
-                # pdb.set_trace()
-                epsilon_greedy_action = self.get_epsilon_greedy_action(
-                    state,
-                    self.__epsilon
-                )
-
-                self._update_dict(state, epsilon_greedy_action)
-
-                state_bar, step_reward, done, info, if_break = self._take_action(
-                    state,
-                    epsilon_greedy_action
-                )
-
-                if if_break:
-                    break
-                    # pass
-                # pdb.set_trace()
-                optimal_action = self.get_optimal_action(state_bar)
-
-                # pdb.set_trace()
-
-                state_greedy_action_Qfunc = self._get_Qfunc(
-                    state,
-                    epsilon_greedy_action
-                )
-
-                state_optimal_action_Qfunc = self._get_Qfunc(
-                    state_bar,
-                    optimal_action
-                )
-
-                # pdb.set_trace()
-                delta = step_reward + self.__gamma * state_optimal_action_Qfunc - \
-                    state_greedy_action_Qfunc
-
-                self.__e = self._update_eligibility_trace(
-                    state,
-                    epsilon_greedy_action
-
-                )
-                # pdb.set_trace()
-                self.__theta = self._update_weights(delta)
-
-                if done:
-                    break
-                # pdb.set_trace()
-                state = state_bar
-
-                total_reward += step_reward
-
-            if total_reward != -self.__max_step_per_episode and not if_break:
-                # self.__alpha *= 0.99
-                print "FuckFuckFuckFuckFuckFuckFuckFuckFuckFuckFuckFuckFuck"
-            # if (num_episode + 1) % 200 == 0:
-            #     pdb.set_trace()
-            # self.__total_reward_history.append(total_reward)
-            # if total_reward > self.__max_total_reward:
-            # self.__max_total_reward = total_reward
-
-            # if (num_episode + 1) % 100 == 1:
-            #     pdb.set_trace()
-
-            if theta_previous.shape == self.__theta.shape:
-                theta_diff = theta_previous - self.__theta
-                norm_theta_diff = np.linalg.norm(theta_diff)
-            if not if_break:
-                print "Episode: ", num_episode
-                print total_reward
-                print self.__dict.shape[0]
-
-                if 'norm_theta_diff' in locals():
-                    print norm_theta_diff
-            # print nortd
-            # self.__theta_diff_history.append(norm_theta_diff)
-
-    def _get_random_action(self):
-        action = rd.choice(self.__action_space)
-        return action
+    # pdb.set_trace()
--- Project/Toolbox/algorithm/__init__.py
+++ Project/Toolbox/algorithm/__init__.py
@@ -1,4 +1,3 @@
 from GGQLambda import GGQLambda
 from RGGQLambda import RGGQLambda
 from OSKQ import OSKQ
-from OSKQ_New import OSKQ_New
